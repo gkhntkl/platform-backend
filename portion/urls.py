397e2e3c09de427bb0d90188c8f8b46efd5335cf
gokhan tekel <32368789+gkhntkl@users.noreply.github.com>
2021-01-24 14:21:00 +0300

    Update urls.py

--- portion/urls.py
+++ portion/urls.py
@@ -2,7 +2,8 @@
 from django.contrib import admin
 from django.urls import path
 from .views import PortionDetailAPIView
+from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
-    path('', PortionDetailAPIView.as_view())
+    path('', csrf_exempt(PortionDetailAPIView.as_view()))
 ]
 
